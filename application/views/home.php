781bb8da7fdc868b574f305a2c43959167071e3c
Amar <Amar@sreedesktop-pc>
2013-05-29 08:51:21 +0530

    initial commit

--- application/views/home.php
+++ application/views/home.php
@@ -1,4 +1,26 @@
-<?php
+<html>
+    <head>
+        <title>Schoolmain - Home</title>
+        <?php  define('URL','http://schoolmain/'); ?>
+        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo URL.'css/schoolmain-main.css';?>" />
+    </head>
+    <body>
+        <div id="sm-header">
+               <?php echo URL.'css/schoolmain-main.css';?>
+        </div>
+        <div id="sm-home-content">
+            <div id="sm-banner">
 
-echo 'hi .. this is the main page now';
-?>
+            </div>
+            <div id="sm-describe">
+
+            </div>
+            <div id="sm-testimonial">
+
+            </div>
+        </div>    
+        <div id="sm-home-footer">
+
+        </div>
+    </body>
+</html>
